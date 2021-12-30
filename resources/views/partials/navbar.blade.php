4a0d7a7e72dd2c234191abc0b2d343fb2328874c
arifbudimanar <arifbudiman01041996@hotmail.com>
2021-12-31 01:51:12 +0700

    user login & middleware

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -21,14 +21,42 @@
                     <a class="nav-link {{ $active === 'about' ? 'active' : '' }}" href="/about">About</a>
                 </li>
             </ul>
+
             <ul class="navbar-nav ms-auto">
-                <li class="nav-item">
-                    <a href="/login" class="nav-link {{ $active === 'login' ? 'active' : '' }}">
-                        <i class="bi bi-box-arrow-in-right"></i>
-                        LOGIN
-                    </a>
-                </li>
-            </ul>
+                @auth
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                            Welcome back, {{ Auth::user()->name }}
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="/dashboard"><i
+                                        class="bi bi-layout-text-sidebar-reverse"></i>
+                                    Dashboard</a></li>
+
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li>
+                                <form action="/logout" method="post">
+                                    @csrf
+                                    <button type="submit" class="dropdown-item"><i class="bi bi-box-arrow-left"></i>
+                                        Logout</button>
+                                </form>
+
+                            </li>
+                        </ul>
+                    </li>
+                @else
+                    <li class="nav-item">
+                        <a href="/login" class="nav-link {{ $active === 'login' ? 'active' : '' }}">
+                            <i class="bi bi-box-arrow-in-right"></i>
+                            LOGIN
+                        </a>
+                    </li>
+                </ul>
+            @endauth
+
         </div>
     </div>
 </nav>
